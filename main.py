997b6d192381c5a0341f25edae40d724687c265d
Grimet14 <gileff.simon@gmail.com>
2023-04-25 21:59:06 -0700

    basic updates

--- main.py
+++ main.py
@@ -1,38 +1,21 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-import aiohttp
- 
-TOKEN = "MTEwMDE2NDI0NjU1NTMzMjY5OQ.GvjjdM.Xn9kYz3zZn7J8O4e5ZnvpEa1swp2gyy9ABZ8lU"
-API_KEY = "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"
+from config import TOKEN
 
-bot = commands.Bot(command_prefix="!", intents = discord.Intents.all())
+bot = commands.Bot(command_prefix='!', intents = discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print("GPT-Bot is up!")
+    print("Grimet-Bot is up!")
     try:
         synced = await bot.tree.sync()
-        print(f"Synced {len(synced)} commands(s)")
+        print(f"Synced {len(synced)} command(s)")
     except Exception as e:
         print(e)
 
-@bot.command()
-async def gpt(ctx: commands.Context, *, prompt: str):
-    async with aiohttp.ClientSession() as session:
-        payload = {
-            "model": "text-davinci-003",
-            "prompt": prompt,
-            "temperature": 0.5,
-            "max_tokens": 10000,
-            "presence_penalty": 0,
-            "frequency_penalty": 0,
-            "best_of": 1,
-        }
-        headers = {"Authorization": f"Bearer {API_KEY}"}
-        async with session.post("https://api.openai.com/v1/completions", json=payload, headers=headers) as resp:
-            response = await resp.json()
-            embed = discord.Embed(title="Chat GPT's Response:", description=response["choices"][0]["text"])
-            await ctx.reply(embed=embed)
+@bot.tree.command(name="hello")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hey {interaction.user.mention}! This is a working slash command" ephemeral=True)
 
 bot.run(TOKEN)
